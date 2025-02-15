63bda3bbba1e06da34465ba59037feb181275fb6
Griffin Reding <griffin_reding@hotmail.com>
2025-02-15 09:37:39 -0700

    initial project creation

---
+++ daily-trivia/daily-trivia/daily_triviaApp.swift
@@ -0,0 +1,32 @@
+//
+//  daily_triviaApp.swift
+//  daily-trivia
+//
+//  Created by GReding on 2/15/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct daily_triviaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
