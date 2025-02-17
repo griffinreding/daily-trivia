2d56fab7fe85670b11e4a0cba8a7c6852f5584c9
Griffin Reding <griffin_reding@hotmail.com>
2025-02-16 21:19:03 -0700

    Update TriviaQuestion.swift

--- daily-trivia/daily-trivia/Models/TriviaQuestion.swift
+++ daily-trivia/daily-trivia/Models/TriviaQuestion.swift
@@ -6,10 +6,10 @@
 //
 
 import Foundation
-import FirebaseFirestoreSwift
+//import FirebaseFirestoreSwift
 
-struct Question: Codable, Identifiable {
-    @DocumentID var id: String { date }
+struct TriviaQuestion: Codable, Identifiable {
+    var id: String { date }
     let question: String
     let choices: [String]?
     let correctAnswer: String
